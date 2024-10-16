9fb693d89ffa5726502176c4eab2098a03e1e6f5
NathanBaraka <nathanbaraka23@gmail.com>
2024-10-16 09:50:58 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const colour=document.getElementById("btn");
+colour.addEventListener("click", ()=>{
+   colour.style.backgroundColor=colour.style.backgroundColor==="brown" ? "yellow" : "green"
+});
